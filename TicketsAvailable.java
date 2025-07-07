af6faedc2aaab542e759dcc091d31cb7f276651a
Vijayabharath1263 <vijayabharathvb1263@gmail.com>
2025-07-07 15:34:39 +0530

    Create TicketsAvailable.java
    OOPS Structured Design

---
+++ TicketsAvailable.java
@@ -0,0 +1,12 @@
+
+public class AvailableTickets {
+
+    public static void availableTickets() {
+        System.out.println("Lower Birth\t\t\t:" + Tickets.available_Lower_Berth);
+        System.out.println("Middle Berth\t\t\t:" + Tickets.available_Middle_Berth);
+        System.out.println("Upper Berth\t\t\t:" + Tickets.available_Upper_Berth);
+        System.out.println("RAC\t\t\t\t:" + Tickets.availabe_RAC);
+        System.out.println("Waiting\t\t\t\t:" + Tickets.available_Waiting);
+    }
+
+}
